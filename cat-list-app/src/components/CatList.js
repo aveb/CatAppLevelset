09f53338104c40b44ae704de1a6e32a5ef0d0ba7
Avery Berkowitz <avery.berkowitz@gmail.com>
2020-01-15 09:35:58 -0600

    create CatList and CatListItem components -remove unnecessary CRA code

---
+++ cat-list-app/src/components/CatList.js
@@ -0,0 +1,7 @@
+import React, { useEffect } from 'react';
+
+export default function CatList(){
+  return (
+    <h1>Cat list here!</h1>
+  )
+}
---
+++ cat-list-app/src/components/CatListItem.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function CatListItem(){
+  return (
+    <h1>CAT!</h1>
+  )
+};
