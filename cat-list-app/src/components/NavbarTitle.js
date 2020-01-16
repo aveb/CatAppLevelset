bd84e44b13ebd6c326dd0d7c7b8283c1c06f5762
Avery Berkowitz <avery.berkowitz@gmail.com>
2020-01-16 11:27:23 -0600

    change NavbarTitle color

--- cat-list-app/src/components/NavbarTitle.js
+++ cat-list-app/src/components/NavbarTitle.js
@@ -2,18 +2,22 @@ import React from "react"
 import { Navbar } from "react-bootstrap"
 
 // styles
-const navStyles = {
+const navContainer = {
   // override container styling
   marginRight: "-15px",
   marginLeft: "-16px"
 }
 
+const navTitle = {
+  color: "white"
+}
+
 export default function NavbarTitle() {
   return (
-    <Navbar inverse staticTop style={navStyles}>
+    <Navbar inverse staticTop style={navContainer}>
       <Navbar.Header>
         <Navbar.Brand>
-          <a href="/">Cats!</a>
+          <a style={navTitle} href="/">Cats!</a>
         </Navbar.Brand>
       </Navbar.Header>
     </Navbar>
